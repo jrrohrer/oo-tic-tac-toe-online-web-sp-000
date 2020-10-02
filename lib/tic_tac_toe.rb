9ae4a8d60484d0f6ca926836284632574cd53b35
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-02 15:55:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,4 +29,8 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0, 8) && !position_taken?(index)
   end
+
+  def turn_count
+    @board.count { |token| token == "X" || token == "O" }
+  end
 end
