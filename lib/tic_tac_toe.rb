9d7bbc519d00fbb2b2e2e803681ad2d548f20277
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-02 16:04:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.detect do |combo|
-      @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[3]] && position_taken?(combo[0])
+      [combo[0] == [combo[1] && [combo[1] == [combo[3] && position_taken?(combo[0])
     end
   end
 end
